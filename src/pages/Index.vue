d32701b3a5f7b53b830631ebedc68e3d740a96b6
Hassan <usgitan@gmail.com>
2018-11-16 16:11:38 +0900

    Add sections

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,37 +15,47 @@
         <a href="https://twitter.com/hassasa3">Twitter</a> / 
         <a href="https://www.instagram.com/hassasasan/">Instagram</a> やっています。
       </p>
-      <h2 class="section-title">仕事 (2018/03/01 ~)</h2>
-      <p>クラウドファンディングのサービスを作っています。</p>
-      <p>主な業務は、バックエンド (Ruby / Rails)・フロントエンド (CSS [SASS] / Javascript) を用いて新規の機能開発や機能改善をすることです。</p>
 
-      <h2 class="section-title">個人開発してます</h2>
-      <p>個人で <a href="https://rinaria.co" target="_blank">Rinaria (https://rinaria.co)</a> という演奏化のマッチングサービスを作っています。</p>
-      <p>質の高いマッチングを実現するため、今は演奏化の動画・音源・文章などのアウトプットをまとめる場所になることを目指しています。</p>
+      <section>
+        <h2 class="section-title">仕事 (2018/03/01 ~)</h2>
+        <p>クラウドファンディングのサービスを作っています。</p>
+        <p>主な業務は、バックエンド (Ruby / Rails)・フロントエンド (CSS [SASS] / Javascript) を用いて新規の機能開発や機能改善をすることです。</p>
+      </section>
+
+      <section>
+        <h2 class="section-title">個人開発してます</h2>
+        <p>個人で <a href="https://rinaria.co" target="_blank">Rinaria (https://rinaria.co)</a> という演奏化のマッチングサービスを作っています。</p>
+        <p>質の高いマッチングを実現するため、今は演奏化の動画・音源・文章などのアウトプットをまとめる場所になることを目指しています。</p>
+      </section>
 
       <br>
       <p>仕事・個人問わず、「自分が欲しくて使うものを作る」ことを大切にしています。</p>
 
-      <h2 class="section-title">ピアノを弾きます</h2>
-      <p>アニソン・ボカロ・JPOP を中心にピアノを弾きます。</p>
-      <p>愛してやまないピアニスト「まらしぃ」さんの譜面を弾いたり、耳コピして弾きます。</p>
-      <p>これは耳コピして弾いた BiSH の「プロミスザスター」という曲です。</p>
-      <iframe width="100%" height="315" src="https://www.youtube.com/embed/7-InFoUHtO8?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <p>使用している電子ピアノは Roland の RD2000 です。</p>
-      <p>最高ですね。</p>
-      <p>今後弾く曲は <a href="https://rinaria.co/users/@uvpian" target="_blank">Rinaria の私のページ</a>から見ることができます。</p>
+      <section>
+        <h2 class="section-title">ピアノを弾きます</h2>
+        <p>アニソン・ボカロ・JPOP を中心にピアノを弾きます。</p>
+        <p>愛してやまないピアニスト「まらしぃ」さんの譜面を弾いたり、耳コピして弾きます。</p>
+        <p>これは耳コピして弾いた BiSH の「プロミスザスター」という曲です。</p>
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/7-InFoUHtO8?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p>使用している電子ピアノは Roland の RD2000 です。</p>
+        <p>最高ですね。</p>
+        <p>今後弾く曲は <a href="https://rinaria.co/users/@uvpian" target="_blank">Rinaria の私のページ</a>から見ることができます。</p>
+      </section>
 
-      <h2 class="section-title">たまに囲碁を打ちます</h2>
-      <p>五段戦で惜しくも準優勝だったり、</p>
-      <g-image class='description-image' src="~/images/igo.png" />
-      <p>小さな大会で優勝するくらいの実力です。</p>
-      <g-image class='description-image' src="~/images/igo_yusyo.png" />
-      <p>囲碁を初めた時から六段の取得が目標なのですが、人生の後半で目標を失った時のために五段でとめています。</p>
+      <section>
+        <h2 class="section-title">たまに囲碁を打ちます</h2>
+        <p>五段戦で惜しくも準優勝だったり、</p>
+        <g-image class='description-image' src="~/images/igo.png" />
+        <p>小さな大会で優勝するくらいの実力です。</p>
+        <g-image class='description-image' src="~/images/igo_yusyo.png" />
+        <p>囲碁を初めた時から六段の取得が目標なのですが、人生の後半で目標を失った時のために五段でとめています。</p>
+      </section>
 
-
-      <h2 class="section-title">今後やってみたいこと</h2>
-      <p>・好きな人が喜ぶサービスを0から作ること</p>
-      <p>・ピアノの生放送</p>
+      <section>
+        <h2 class="section-title">今後やってみたいこと</h2>
+        <p>・好きな人が喜ぶサービスを0から作ること</p>
+        <p>・ピアノの生放送</p>
+      </section>
     </article>
   </Layout>
 </template>
